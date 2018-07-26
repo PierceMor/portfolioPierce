baa87b2c6c0710e16b806d723e7e397f85240dca
Pierce Morrill <piercemorrill91@gmail.com>
2018-07-26 17:42:18 -0500

    typos..

--- src/components/card.js
+++ src/components/card.js
@@ -66,7 +66,7 @@ function MediaCard(props) {
                     </Typography>
                     
                     <Typography variant="subheading"     color="textSecondary">
-                        My name is Pierce Morrill, I a refugee from Dallas trying to make a life for myself in the great land of Austin. My Academic career was focused primarily on Anthropology, Linguistics, and German Studies. After School I began to look for something more meaningful and I became a bit stranded just waiting tables for a while. I came to Austin last year to work as a translator for a gaming company. 
+                        My name is Pierce Morrill, hailing from the Glorious land of the DFW to conquer this beautiful Ausstin country. My Academic career was focused primarily on Anthropology, Linguistics, and German Studies. After School I began to look for something more meaningful and I became a bit stranded just waiting tables for a while. I came to Austin last year to work as a translator for a gaming company. 
                     </Typography>
                 </CardContent>
             </div>
